c98eb1978f1994edd49746b1a871a998ffaae49e
Hila Vasiler <hila.marmel@gmail.com>
2016-03-26 18:33:32 -0700

    added comments and made the code DRY

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 $(document).on("ready", function(){
   console.log("JS working!");
-  //create new player
-  player1 = new Player('Hila', 'A');
-  player2= new Player('Emily', 'L');
+  //create two new players
+  player1 = new Player('A');
+  player2= new Player('L');
   var finish=false;
   board = new Board();
-  //listen to keyUP event and animate move
+  //listen to keydown event
   $(document).on("keydown", function(event){
-    if(event.which===65 && finish===false){
-      player1.move($("#player1"));
-      if(player1.checkWin("#player1")) {
+    var p;
+    //check if the game is still on
+    if(finish===false){
+      //check for key 'A'-->player1's key
+      if(event.which===65){
+        p=player1;
+      }
+      //check for the key 'L'-->player2's key
+      else if(event.which===76){
+        p=player2;
+      }
+      //call the move function
+      p.move($("#" + p));
+      //call the checkWin function
+      if(p.checkWin()) {
         finish = true;
-          alert("Game Over - The winner is player 1");
+        alert("Game Over - The winner is: " + p.name);
       }
     }
-    else if(event.which===76 && finish===false){
-      player2.move($("#player2"));
-      if(player2.checkWin("#player2")){
-        finish = true;
-        alert("Game Over - The winner is player 2");
-      }
-    }
+    // if(event.which===65 && finish===false){
+    //   player1.move($("#player1"));
+    //   if(player1.checkWin("#player1")) {
+    //     finish = true;
+    //       alert("Game Over - The winner is: " + player1.name);
+    //   }
+    // }
+    // else if(event.which===76 && finish===false){
+    //   player2.move($("#player2"));
+    //   if(player2.checkWin("#player2")){
+    //     finish = true;
+    //     alert("Game Over - The winner is: " + player2.naem);
+    //   }
+  //  }
   });
+  //listen to reset button click event
   $('button').on("click", function(){
     board.reset();
   });
@@ -29,6 +49,7 @@ $(document).on("ready", function(){
 });
 
 function Board(){
+  //reset function - reset margins to 30-->brings the player to the start line
   this.reset = function(){
     player1.pWidth = 30;
     player2.pWidth = 30;
@@ -38,20 +59,22 @@ function Board(){
   };
 }
 
-function Player(){
-  this.name = "Hila";
-  this.moveKey = "A";
+function Player(moveKey){
+  //this.name = pName;
+  this.moveKey = moveKey;
   this.pWidth = 30;
 
-  //animate move on every keyUp event
-  this.move = function($player){
-    $player.animate({ "margin-left": "+=30px" }, "fast");
+  //animate move on every keydown event--> increase margin-left by 30px every move
+  this.move = function(){
+    this.animate({ "margin-left": "+=30px" }, "fast");
+    //record the total left margin
     this.pWidth +=30;
     //console.log(this.pWidth);
   };
 
-  this.checkWin = function($player){
+  this.checkWin = function(){
     //alert("player width: " + this.pWidth + " window width: " + window.innerWidth);
+    //check if the total left margin is greater the window inner width
     if(this.pWidth>=window.innerWidth-60){
         return true;
     }
